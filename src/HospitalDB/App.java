d817b216df520059b9e1c1c597fda23d42018065
zrygan <zr.gntn@gmail.com>
2024-09-24 15:22:41 +0800

    Added SQL connection in the App

--- src/HospitalDB/App.java
+++ src/HospitalDB/App.java
@@ -1,9 +1,25 @@
 package HospitalDB;
 
+import java.sql.*;
+
+/**
+ * App class, contains main function
+ */
 public class App {
-   
-   
-   public static void main(String[] args) {
-    System.out.println("DB App");    
-   } 
+
+    /**
+     *   
+     */
+    static final String _DURL  = "";
+    static final String _USER  = "";
+    static final String _PASS  = "";
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(_DURL, _USER, _PASS)) {
+            System.out.println("DB App");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
