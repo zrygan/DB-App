23a0ebbe0479589c6fb4f5a8c128b0a3ffb50b48
zrygan <zr.gntn@gmail.com>
2024-09-28 10:06:11 +0800

    Added SQL and Java for creating a new medical record for a patient.

--- src/HospitalDB/Medical.java
+++ src/HospitalDB/Medical.java
@@ -1,12 +1,18 @@
 package HospitalDB;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
- * Class for Medical Report
+ * Class for Medical Report By Zhean Ganituen
  */
 public class Medical {
 
+    private final App DATABASE;
+    private final Patient PATIENT;
     /**
      * the chief complaint of the patient. Example: "difficulty breathing"
      */
@@ -57,8 +63,10 @@ public class Medical {
     private LocalDateTime date_time;
 
     /**
-     * Constructor for Medical class.
+     * Constructor for Medical class. This is also the record creating method.
      *
+     * @param DATABASE the database instance
+     * @param PATIENT the patient who "owns" this medical record
      * @param chief_complaint the chief complaint of the patient
      * @param reason_for_admission the reason for admission of the patient
      * @param medical_diagnosis the final diagnosis of the patient
@@ -69,21 +77,48 @@ public class Medical {
      * @param spo2 the amount of oxygen in the patient's blood
      * @param date_time the date and time the Medical Record is recorded
      */
-    public Medical(String chief_complaint, String reason_for_admission, String medical_diagnosis, int temperature, int pulse, int BP_systolic, int BP_diastolic, int spo2, LocalDateTime date_time) {
+    public Medical(App DATABASE, Patient PATIENT, String chief_complaint, String reason_for_admission, String medical_diagnosis, int temperature, int pulse, int BP_systolic, int BP_diastolic, int spo2, LocalDateTime date_time) {
+        this.DATABASE = DATABASE;
+        this.PATIENT = PATIENT;
+
+        // verify if the patient record is admitted
+        // if not change it to admitted
+        if (!(PATIENT.getStatus().equals("admitted"))) {
+            PATIENT.setStatus("admitted");
+        }
+
         this.chief_complaint = chief_complaint;
-        this.reason_for_admission = reason_for_admission;
-        this.medical_diagnosis = medical_diagnosis;
         this.temperature = temperature;
         this.pulse = pulse;
         this.BP_systolic = BP_systolic;
         this.BP_diastolic = BP_diastolic;
         this.spo2 = spo2;
         this.date_time = date_time;
+
+        // check if the reason for admission and diagnosis is NULL
+        // if it is then set it to the previous RFA and Diagnosis of the patient
+        // otherwise, set a different one
+        if (reason_for_admission == null) {
+            // the most recent (or previous medical record of the patient) is the last one
+            // scary single liner
+            this.reason_for_admission = PATIENT.getMEDICAL().get(PATIENT.getMEDICAL().size() - 1).getReasonForAdmission();
+        } else {
+            this.reason_for_admission = reason_for_admission;
+        }
+
+        if (medical_diagnosis == null) {
+            this.medical_diagnosis = PATIENT.getMEDICAL().get(PATIENT.getMEDICAL().size() - 1).getMedicalDiagnosis();
+        } else {
+            this.medical_diagnosis = medical_diagnosis;
+        }
+
+        // add this new medical record to the array of medical records assigned to the patient
+        addMEDICAL();
     }
 
     // Getters
     /**
-     * Gets the chief complaint.
+     * Gets chief complaint.
      *
      * @return the chief complaint
      */
@@ -227,21 +262,38 @@ public class Medical {
         this.date_time = date_time;
     }
 
-    /**
-     * MedicalRecord_create creates a medical record
-     *
-     * FIXME: This might need to be a static method
+    /** 
+     * Creating a medical record in  the SQL database
      */
-    public void create() {
+    public void create(Connection conn) {
+        String sql = "INSERT INTO MedicalRecords (chief_complaint, reason_for_admission, medical_diagnosis, temperature, pulse, BP_systolic, BP_diastolic, spo2, date_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, chief_complaint);
+            pstmt.setString(2, reason_for_admission);
+            pstmt.setString(3, medical_diagnosis);
+            pstmt.setInt(4, temperature);
+            pstmt.setInt(5, pulse);
+            pstmt.setInt(6, BP_systolic);
+            pstmt.setInt(7, BP_diastolic);
+            pstmt.setInt(8, spo2);
+            pstmt.setObject(9, date_time);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     /**
-     * updates an already existing medical record
+     * Updating an already existing medical record For updating vital signs,
      *
      * FIXME: This might need to be a static method
      */
     public void update() {
+        /*
+
+         */
 
     }
 
@@ -251,6 +303,9 @@ public class Medical {
      * FIXME: This might need to be a static method
      */
     public void delete() {
+        /*
+         
+         */
 
     }
 
@@ -274,5 +329,15 @@ public class Medical {
      */
     public void viewSummary() {
 
+    }
+
+    // Helper functions
+    /**
+     * Adds this medical record to the list of medical records of the patient
+     */
+    private void addMEDICAL() {
+        ArrayList<Medical> medicalRecords = PATIENT.getMEDICAL();
+        medicalRecords.add(this);
+        PATIENT.setMEDICAL(medicalRecords);
     }
 }
