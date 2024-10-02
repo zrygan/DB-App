b42e842c4247953ecb5a3ddb96cae6d1abf327eb
zrygan <zr.gntn@gmail.com>
2024-10-02 18:26:45 +0800

    Deleted helper files

--- src/com/HospitalDB/App.java
+++ src/com/HospitalDB/App.java
@@ -1,19 +1,40 @@
 package com.HospitalDB;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 
 public class App {
 
-    static final String _DURL  = "jdbc:mysql://localhost:3306/HOSPITAL_DB";
-    static final String _USER  = "root";
-    static final String _PASS  = "password";
+    private static final String DURL  = "jdbc:mysql://127.0.0.1:3306/Hospital_DB";
+    private static final String USER  = "root";
+    private  static final String PASS  = "password";
     
     public static void main(String[] args) {
-        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        try (Connection conn = DriverManager.getConnection(_DURL, _USER, _PASS)) {
+        Scanner sc = new Scanner(System.in);
+
+        try (Connection connection = DriverManager.getConnection(DURL, USER, PASS)) {
             System.out.println("Connected.");
+            
+            String query;
+
+            System.out.print("Query << ");
+            query = sc.nextLine();
+            
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            while(resultSet.next()){
+                System.out.println(query + " << " + resultSet.getString("doctor_name") + " : " + resultSet.getString("doctor_specialization"));
+            }
+
+            sc.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
