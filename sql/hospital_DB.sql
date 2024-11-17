718cadd1e8b32013d1a63ad1686e13cc1d918421
zrygan <zr.gntn@gmail.com>
2024-11-17 14:14:21 +0800

    Updated sql

--- sql/hospital_DB.sql
+++ sql/hospital_DB.sql
@@ -4,52 +4,89 @@ USE Hospital_DB;
 
 CREATE TABLE IF NOT EXISTS patients_record (
     patient_ID INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    age INT,
-    birth_date DATE,
-    sex ENUM('MALE', 'FEMALE'),
-    height DECIMAL(5, 2),         -- Height in centimeters or inches
-    weight DECIMAL(5, 2),         -- Weight in kilograms or pounds
-    religion VARCHAR(50),
-    doctor VARCHAR(100),
-    status ENUM('ADMITTED', 'DISCHARGED'),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    patient_name VARCHAR(100)    NOT NULL,
+    age INT              NOT NULL,
+    birth_date DATE      NOT NULL,
+    sex ENUM('M', 'F')   NOT NULL,
+    patient_height DECIMAL(5, 2) NOT NULL,         -- Height in centimeters or inches
+    patient_weight DECIMAL(5, 2) NOT NULL,         -- Weight in kilograms or pounds
+    religion VARCHAR(50) NOT NULL,
+    doctor INT FOREIGN KEY NOT NULL REFERENCES doctors_record(doctor_ID),
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS doctors_record (
-    doctor_ID VARCHAR(100) PRIMARY KEY,
-    name VARCHAR(100),
-    specialization VARCHAR(100),
-    phoneNumber VARCHAR(100),
-    email VARCHAR(100),
-    medicalHierarchy VARCHAR(100)
+    doctor_ID INT PRIMARY KEY,
+    doctor_name VARCHAR(100)      NOT NULL,
+    specialization VARCHAR(100)   NOT NULL,
+    phoneNumber VARCHAR(100)      NOT NULL,
+    email VARCHAR(100)            NOT NULL,
+    medicalHierarchy VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS medication_record (
-    medication_ID INT PRIMARY KEY AUTO_INCREMENT,
-    doctor_ID VARCHAR(100),
-    patient_ID INT,
+CREATE TABLE IF NOT EXISTS consultation_record(
+    consultation_ID INT PRIMARY KEY,
+    prescription_ID INT FOREIGN KEY REFERENCES prescription_record(prescription_ID),
+    doctor_ID INT FOREIGN KEY REFERENCES doctors_record(doctor_ID),
+    patient_ID INT FOREIGN KEY REFERENCES patients_record(patient_ID),
+    vital_signs_ID INT FOREIGN KEY REFERENCES vital_signs_record(vital_signs_ID),
+    lab_report_ID INT FOREIGN KEY REFERENCES lab_report_record(lab_report_ID),
+    consultation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS medication_record(
+    medication_ID INT PRIMARY KEY,
     generic_name VARCHAR(100),
-    brand_name VARCHAR(100),
-    date_time DATETIME,
-    frequency VARCHAR(100), -- e.g., every day, every week, every 8 hours
-    dosage FLOAT, -- in milliliters (e.g. 10 = 10 mL)
-    FOREIGN KEY (doctor_ID) REFERENCES doctors_record(doctor_ID),
-    FOREIGN KEY (patient_ID) REFERENCES patients_record(patient_ID)
+    brand_name VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS medical_record (
-    record_ID INT PRIMARY KEY AUTO_INCREMENT,
-    patient_ID INT,
-    chief_complaint VARCHAR(255),
-    reason_for_admission VARCHAR(100),
-    medical_diagnosis VARCHAR(100),
-    temperature DECIMAL(3,2), -- temperature of patient (e.g., 37.00)
+CREATE TABLE IF NOT EXISTS prescription_record(
+    prescription_ID INT PRIMARY KEY,
+    medication_ID INT FOREIGN KEY REFERENCES medication_record(medication_ID),
+    prescription_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    frequency INT,
+    dosage DECIMAL(5,5),                         -- in grams
+    doctor_ID INT FOREIGN KEY REFERENCES doctors_record(doctor_ID),
+    patient_ID INT FOREIGN KEY REFERENCES patients_record(patient_ID)
+);
+
+CREATE TABLE IF NOT EXISTS vital_signs_record(
+    vital_signs_ID INT PRIMARY KEY,
+    temperature DECIMAL(2,2),                    -- temperature cannot exceed 3 digits (unrealistic)
     pulse INT,
-    BP_systolic INT,
-    BP_diastolic INT,
-    spo2 INT,
-    date_time DATETIME,
-    FOREIGN KEY (patient_ID) REFERENCES patients_record(patient_ID)
+    respiratory_rate INT,
+    systolic_bp INT,
+    diastolic_bp INT,
+    SPO2 INT,
+    vital_signs_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS lab_report_record(
+    lab_report_ID INT PRIMARY KEY,
+    test_ID INT FOREIGN KEY
+);
+
+CREATE TABLE IF NOT EXISTS lab_test_record(
+    test_ID INT PRIMARY KEY,
+    test_description VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS consultation_chief_complaint_record(
+    consultation_ID INT PRIMARY KEY,
+    complaint_ID INT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS chief_complaint_record(
+    complaint_ID INT PRIMARY KEY,
+    complaint_description VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS consultation_medical_diagnosis_record(
+    consultation_ID INT PRIMARY KEY,
+    diagnosis_ID INT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS medical_diagnosis_record(
+    diagnosis_ID INT PRIMARY KEY,
+    diagnosis_description VARCHAR(255)
 );
