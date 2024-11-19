dfb883a4a4039f2e842cb942abc5de95790200a8
jazjimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2024-11-19 11:48:18 +0800

    Fixed App.java

--- src/main/java/com/source/HospitalDB/App.java
+++ src/main/java/com/source/HospitalDB/App.java
@@ -8,10 +8,10 @@ import main.java.com.source.HospitalDB.DAO.DoctorsDAO;
 public class App {
     
     public static void main(String[] args) {
-        Doctors zhean = new Doctors("12345678", "Zhean", "Cardiologist", "0917222", "zhean@hospital.com", "Senior Consultant");
+        Doctors zhean = new Doctors("12345678", "Zhean", "Cardiologist", "0917222", "zhean@hospital.com");
          
         try {
-            DoctorsDAO.addDoctors(zhean);
+            DoctorsDAO.create(zhean);
         } catch (SQLException e) {
             System.err.println("Error adding doctor" + e.getMessage()); 
         }
